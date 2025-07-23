0d2a9fdd843387ad92d1d3493d097cedc326ff0d
Raul Ronald <108026640+RaulRonald@users.noreply.github.com>
2025-07-23 16:53:22 -0300

    Usa HashRouter para corrigir rotas no GitHub Pages

--- dinodex-react/src/App.jsx
+++ dinodex-react/src/App.jsx
@@ -1,23 +1,22 @@
 // src/App.jsx
 
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Trocamos BrowserRouter por HashRouter para funcionar no GitHub Pages
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import GuessDinoPage from './pages/GuessDinoPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="text-stone-200">
         <Routes>
-          {/* Rota para a página inicial */}
+          {/* A definição das rotas continua a mesma */}
           <Route path="/" element={<HomePage />} />
-          
-          {/* Rota para a página do minigame */}
           <Route path="/guess" element={<GuessDinoPage />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
